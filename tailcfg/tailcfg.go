4437504df038a4a51c3c6d5dbfbe3ee8cdccb91d
Gesa Stupperich <gesa@tailscale.com>
2026-02-17 13:15:02 +0000

    tailcfg: reintroduce UserProfile.Groups
    This change reintroduces UserProfile.Groups, a slice that contains
    the ACL-defined and synced groups that a user is a member of.
    
    The slice will only be non-nil for clients with the node attribute
    see-groups, and will only contain groups that the client is allowed
    to see as per the app payload of the see-groups node attribute.
    
    For example:
    ```
    "nodeAttrs": [
      {
        "target": ["tag:dev"],
        "app": {
          "tailscale.com/see-groups": [{"groups": ["group:dev"]}]
        }
      },
    
      [...]
    
    ]
    ```
    
    UserProfile.Groups will also be gated by a feature flag for the time
    being.
    
    Updates tailscale/corp#31529
    
    Signed-off-by: Gesa Stupperich <gesa@tailscale.com>

--- tailcfg/tailcfg.go
+++ tailcfg/tailcfg.go
@@ -179,7 +179,8 @@ type CapabilityVersion int
 //   - 130: 2025-10-06: client can send key.HardwareAttestationPublic and key.HardwareAttestationKeySignature in MapRequest
 //   - 131: 2025-11-25: client respects [NodeAttrDefaultAutoUpdate]
 //   - 132: 2026-02-13: client respects [NodeAttrDisableHostsFileUpdates]
-const CurrentCapabilityVersion CapabilityVersion = 132
+//   - 133: 2023-07-23: UserProfile.Groups readded (available via WhoIs) (removed in 87)
+const CurrentCapabilityVersion CapabilityVersion = 133
 
 // ID is an integer ID for a user, node, or login allocated by the
 // control plane.
@@ -281,6 +282,13 @@ type UserProfile struct {
 	LoginName     string // "alice@smith.com"; for display purposes only (provider is not listed)
 	DisplayName   string // "Alice Smith"
 	ProfilePicURL string `json:",omitzero"`
+
+	// Groups contains group identifiers for any group that this user is
+	// a part of and that the coordination server is configured to tell
+	// your node about. (Thus, it may be empty or incomplete.)
+	// There's no semantic difference between a nil and an empty list.
+	// The list is always sorted.
+	Groups []string `json:",omitempty"`
 }
 
 func (p *UserProfile) Equal(p2 *UserProfile) bool {
@@ -293,7 +301,8 @@ func (p *UserProfile) Equal(p2 *UserProfile) bool {
 	return p.ID == p2.ID &&
 		p.LoginName == p2.LoginName &&
 		p.DisplayName == p2.DisplayName &&
-		p.ProfilePicURL == p2.ProfilePicURL
+		p.ProfilePicURL == p2.ProfilePicURL &&
+		(len(p.Groups) == 0 && len(p2.Groups) == 0 || reflect.DeepEqual(p.Groups, p2.Groups))
 }
 
 // RawMessage is a raw encoded JSON value. It implements Marshaler and
